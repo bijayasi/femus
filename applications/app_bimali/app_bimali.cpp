519fa5da53b6aae733c62832eb37b4528ad48d31
Bimali Jayasinghe <student@mam377-l.ttu.edu>
2015-11-12 12:31:58 -0600

    fixed runtime U variable error

--- applications/app_bimali/app_bimali.cpp
+++ applications/app_bimali/app_bimali.cpp
@@ -65,10 +65,11 @@ int main(int argc, char** args){
 
   // add variables to mlSol
   mlSol.AddSolution("U", LAGRANGE, FIRST);
+  mlSol.Initialize("All");    // initialize all varaibles to zero
   
   // attach the boundary condition function and generate boundary data
       mlSol.AttachSetBoundaryConditionFunction(SetBoundaryCondition);
-      mlSol.GenerateBdc("u");
+//       mlSol.GenerateBdc("U");
 
       // define the multilevel problem attach the mlSol object to it
       MultiLevelProblem mlProb(&mlSol);
@@ -76,8 +77,8 @@ int main(int argc, char** args){
       // add system Poisson in mlProb as a Linear Implicit System
       LinearImplicitSystem& system = mlProb.add_system < LinearImplicitSystem > ("Poisson");
 
-      // add solution "u" to system
-      system.AddSolutionToSystemPDE("u");
+      // add solution "U" to system
+      system.AddSolutionToSystemPDE("U");
 
       // attach the assembling function to system
       system.SetAssembleFunction(AssemblePoissonProblem);
@@ -86,7 +87,7 @@ int main(int argc, char** args){
       system.init();
       system.solve();
  
-  mlSol.Initialize("All");    // initialize all varaibles to zero
+  
 
 //   mlSol.Initialize("U", InitalValueU);
   
@@ -141,11 +142,11 @@ void AssemblePoissonProblem(MultiLevelProblem& ml_prob) {
 
   //solution variable
   unsigned soluIndex;
-  soluIndex = mlSol->GetIndex("u");    // get the position of "u" in the ml_sol object
-  unsigned soluType = mlSol->GetSolutionType(soluIndex);    // get the finite element type for "u"
+  soluIndex = mlSol->GetIndex("U");    // get the position of "U" in the ml_sol object
+  unsigned soluType = mlSol->GetSolutionType(soluIndex);    // get the finite element type for "U"
 
   unsigned soluPdeIndex;
-  soluPdeIndex = mlPdeSys->GetSolPdeIndex("u");    // get the position of "u" in the pdeSys object
+  soluPdeIndex = mlPdeSys->GetSolPdeIndex("U");    // get the position of "U" in the pdeSys object
 
   vector < double >  solu; // local solution
   solu.reserve(maxSize);
